a0b9ce5ba08ebc9fe5aa1d233840706d91ff5d18
Micah <micahkimathi@gmail.com>
2019-05-23 09:18:46 +0300

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,34 @@
+import os
+
+class Config:
+
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://micah:kimamire@localhost/watchlist'
+
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config : the parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+
+
+
+config_options={
+    'development':DevConfig,
+    'production':ProdConfig
+}
